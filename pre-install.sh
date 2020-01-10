004ea2c4372b32c8e81f1baf48fcdebd4db05525
Stew Wilson <stew.wilson@ed.ac.uk>
2020-01-10 11:46:44 +0000

    Add pre-install.sh

---
+++ pre-install.sh
@@ -0,0 +1,8 @@
+echo 'Installing oh-my-zsh'
+/bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir -p ~/.oh-my-zsh/custom/themes
+mkdir -p ~/.oh-my-zsh/custom/plugins
+
+echo 'Installing zsh plugins'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
