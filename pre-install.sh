c5fef86d2a7a8b99586d565bf1660e81a79638cd
Stew Wilson <stew.wilson@ed.ac.uk>
2020-01-16 15:34:56 +0000

    Update theme, packagelist, & pre-install hook

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,5 @@
 echo 'Installing oh-my-zsh'
-/bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+/bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 mkdir -p ~/.oh-my-zsh/custom/themes
 mkdir -p ~/.oh-my-zsh/custom/plugins
 
