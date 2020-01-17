01ff386c7c775e0b74e8e0b9e618b837e042c2fa
Stew Wilson <stew.wilson@ed.ac.uk>
2020-01-17 12:19:25 +0000

    Integrate zsh-apple-touchbar

--- pre-install.sh
+++ pre-install.sh
@@ -4,6 +4,9 @@ echo 'Installing oh-my-zsh'
 /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 mkdir -p ~/.oh-my-zsh/custom/themes
 
+echo 'Installing zsh-apple-touchbar'
+git clone https://github.com/vegerot/zsh-apple-touchbar.git -b autoGenerate $ZSH_CUSTOM/plugins/zsh-apple-touchbar
+
 echo 'Fixing Quicklook plugins'
 xattr -dr com.apple.quarantine ~/Library/Quicklook/QLMarkdown.qlgenerator
 xattr -dr com.apple.quarantine ~/Library/Quicklook/QLStephen.qlgenerator
